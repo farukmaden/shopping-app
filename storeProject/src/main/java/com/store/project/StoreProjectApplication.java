295ede6020aae33889b92da3fc8c7f8fee625828
farukmaden <77931457+farukmaden@users.noreply.github.com>
2021-03-30 22:50:58 +0300

    Create StoreProjectApplication.java

---
+++ storeProject/src/main/java/com/store/project/StoreProjectApplication.java
@@ -0,0 +1,13 @@
+package com.store.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreProjectApplication.class, args);
+	}
+
+}
